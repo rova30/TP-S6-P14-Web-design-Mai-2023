9828e201a5dad455d8bacdaadfdb50c54fa00a1e
rova30 <rovarobel30@gmail.com>
2023-05-06 23:27:30 +0300

    Update AppServiceProvider.php

--- tp-s6-p14-web-design-mai-2023/app/Providers/AppServiceProvider.php
+++ tp-s6-p14-web-design-mai-2023/app/Providers/AppServiceProvider.php
@@ -20,8 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if(request()->server('HTTP_ACCEPT_ENCODING') && strpos(request()->server('HTTP_ACCEPT_ENCODING'),
-            'gzip') !== false) {
+        if(request()->server('HTTP_ACCEPT_ENCODING') && str_contains(request()->server('HTTP_ACCEPT_ENCODING'), 'gzip')) {
             ob_start("ob_gzhandler");
         }
     }
