d06ba1815ba482d71c57750bd246b360db7b642c
rova30 <rovarobel30@gmail.com>
2023-05-07 11:15:50 +0300

    optim bonne pratique

--- tp-s6-p14-web-design-mai-2023/config/app.php
+++ tp-s6-p14-web-design-mai-2023/config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tp-s6-p14-web-design-mai-2023-by-rova1667.up.railway.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
