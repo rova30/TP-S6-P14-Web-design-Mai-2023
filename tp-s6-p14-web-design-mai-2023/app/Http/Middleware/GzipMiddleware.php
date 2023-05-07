b272c29d270adbd9b410a66ce08d80325e55d5e9
rova30 <rovarobel30@gmail.com>
2023-05-07 18:45:31 +0300

    gzip

---
+++ tp-s6-p14-web-design-mai-2023/app/Http/Middleware/GzipMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Response;
+
+class GzipMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response instanceof Response) {
+            $content = gzencode($response->getContent(), 9);
+            $response->setContent($content);
+            $response->header('Content-Encoding', 'gzip');
+            $response->header('Vary', 'Accept-Encoding');
+        }
+
+        return $response;
+    }
+}
